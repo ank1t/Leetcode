7b7cbf81dc2abe9578b332d1288fe13d12eeb2d8
Ankit Singh <159.ank159@gmail.com>
2026-06-21 14:46:36 -0400

    Brute force for deleting nth node from LL

--- LinkedList/DeleteNthNodeFromEndOfLL.java
+++ LinkedList/DeleteNthNodeFromEndOfLL.java
@@ -10,7 +10,7 @@ class Node {
 }
 
 class Solution {
-    Node convertArrayToLinkedList(int[] arr) {
+    Node convertArrayToLL(int[] arr) {
         Node head = new Node(arr[0]);
         Node temp = head;
         for(int k = 1;k<arr.length;k++) {
@@ -29,8 +29,31 @@ class Solution {
         }
     }
 
-    Node deleteNThNodeFromEndOfLL(Node head, int n) {
+    Node deleteNThNodeFromEndOfLLBruteForce(Node head, int n) {
+        if(head == null || (head.next == null && n == 1) ) return null;
 
+        int count = 0;
+        Node temp = head;
+        while(temp != null) {
+            count++;
+            temp = temp.next;
+        }
+
+        if(n == count) return head.next;
+        temp = head;
+        while(temp.next != null) {
+            if(count - n == 1) {
+                temp.next = temp.next.next;
+                break;
+            }
+            count--;
+            temp = temp.next;
+        }
+        return head;
+    }
+
+    Node deleteNThNodeFromEndOfLLOptimized(Node head, int n) {
+    return null;
     }
 }
 class Scratch {
@@ -38,6 +61,7 @@ class Scratch {
         Solution sol = new Solution();
         int[] arr = {1,2,3,4,5,6,7,8,9,0};
         Node head = sol.convertArrayToLL(arr);
-        sol.printLLFromHead(head);
+        Node newHead = sol.deleteNThNodeFromEndOfLLBruteForce(head,5);
+        sol.printLLFromHead(newHead);
     }
 }
