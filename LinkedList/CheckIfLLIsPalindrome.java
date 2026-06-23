7b37a0ef02edc891734bc03b4b305e47c0353e89
Ankit Singh <159.ank159@gmail.com>
2026-06-23 19:56:59 -0400

    Starting funcs added for palindrome check

---
+++ LinkedList/CheckIfLLIsPalindrome.java
@@ -0,0 +1,35 @@
+class Node {
+    int val;
+    Node next;
+
+    Node(int data) {
+        val = data;
+        next = null;
+    }
+}
+
+class Solution {
+    Node convertArrayToLL(int[] arr) {
+        Node head = new Node(arr[0]);
+        Node temp = head;
+        for(int k = 1;k< arr.length;k++) {
+            Node newNode = new Node(arr[k]);
+            temp.next = newNode;
+            temp = newNode;
+        }
+        return head;
+    }
+
+    void printLLFromHeadToTail(Node head) {
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+    }
+}
+class Scratch {
+    public static void main(String[] args) {
+        
+    }
+}
