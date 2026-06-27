a2761cede997c229871c9fbce3ec577e13c1b70c
Ankit Singh <159.ank159@gmail.com>
2026-06-27 16:47:52 -0400

    Rotate LL final implementation

--- LinkedList/RotateLL.java
+++ LinkedList/RotateLL.java
@@ -29,12 +29,41 @@ class Solution {
             temp = temp.next;
         }
     }
+
+    Node rotateLL(Node head, int rIndex) {
+        if(head == null || head.next == null) return head;
+
+        int count = 1;
+        Node temp = head;
+
+        while(temp.next != null) {
+            count++;
+            temp = temp.next;
+        }
+        if(rIndex % count == 0) return head;
+        temp.next = head;
+        rIndex = rIndex % count;
+        int newCount = 0;
+        temp = head;
+
+        while(temp != null) {
+            newCount++;
+            if(count - newCount == rIndex) {
+                Node t = temp.next;
+                temp.next = null;
+                return t;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
 }
 class Scratch {
     public static void main(String[] args) {
         Solution sol = new Solution();
         int[] arr = {1,2,3,4,5,6,7};
         Node head = sol.convertArrayToLL(arr);
-        sol.printLLFromHeadToTail(head);
+        Node newHead = sol.rotateLL(head, 8);
+        sol.printLLFromHeadToTail(newHead);
     }
 }
