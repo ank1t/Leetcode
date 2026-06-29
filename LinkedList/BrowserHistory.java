02e2f68efe8d88b398754926c5d3745f0e4dceb7
Ankit Singh <159.ank159@gmail.com>
2026-06-29 14:07:25 -0400

    Browser History implementation

--- LinkedList/BrowserHistory.java
+++ LinkedList/BrowserHistory.java
@@ -1,19 +1,35 @@
 class BrowserHistory {
 
-    public BrowserHistory(String homepage) {
+    private Node list;
+    private Node currNode;
 
+    public BrowserHistory(String homepage) {
+        Node home = new Node(homepage);
+        currNode = home;
+        list = home;
     }
 
     public void visit(String url) {
-
+        Node newNode = new Node(url);
+        currNode.next = newNode;
+        newNode.prev = currNode;
+        currNode = newNode;
     }
 
     public String back(int steps) {
-
+        while(currNode.prev != null && steps > 0) {
+            currNode = currNode.prev;
+            steps--;
+        }
+        return currNode.data;
     }
 
     public String forward(int steps) {
-
+        while(currNode.next != null && steps > 0) {
+            currNode = currNode.next;
+            steps--;
+        }
+        return currNode.data;
     }
 }
 
@@ -31,6 +47,15 @@ class Node {
 
 class Scratch {
     public static void main(String[] args) {
-
+        BrowserHistory obj = new BrowserHistory("google.com");
+        obj.visit("ig");
+        obj.visit("fb");
+        obj.visit("yt");
+        obj.visit("pinterest");
+        obj.visit("reddit.com");
+        System.out.println(obj.back(4));
+        System.out.println(obj.forward(4));
+        obj.visit("wiki");
+        System.out.println(obj.back(1));
     }
 }
