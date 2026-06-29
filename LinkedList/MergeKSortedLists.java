082b45860df34a802d4ddffbac31e88e55136503
Ankit Singh <159.ank159@gmail.com>
2026-06-29 14:22:40 -0400

    Merge K sorted lists starter code

---
+++ LinkedList/MergeKSortedLists.java
@@ -0,0 +1,41 @@
+
+class Node {
+    Node next;
+    int val;
+
+    Node(int data) {
+        val = data;
+        next = null;
+    }
+}
+
+class Solution {
+    Node convertArrayToLL(int[] arr) {
+        Node head = new Node(arr[0]);
+        Node temp = head;
+
+        for(int k = 1;k < arr.length;k++) {
+            Node newNode = new Node(arr[k]);
+            temp.next = newNode;
+            temp = newNode;
+        }
+        return head;
+    }
+
+    void printLLFromHeadToTail(Node head) {
+        Node temp = head;
+        while(temp  != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+    }
+}
+
+class Scratch {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] arr = {1,2,3,4,5,6,7,8,9,0};
+        Node head = sol.convertArrayToLL(arr);
+        sol.printLLFromHeadToTail(head);
+    }
+}
