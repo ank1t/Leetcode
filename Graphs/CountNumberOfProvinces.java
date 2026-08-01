0198110316a652bae07832f993fbaacbe572d7cf
Ankit Singh <159.ank159@gmail.com>
2026-08-01 16:45:27 -0400

    Optimized version without using Adj list

--- Graphs/CountNumberOfProvinces.java
+++ Graphs/CountNumberOfProvinces.java
@@ -3,41 +3,26 @@ import java.util. *;
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected[0].length;
-        ArrayList<ArrayList<Integer>> adj = convertToAdjList(isConnected);
         boolean[] visited = new boolean[n];
         int count = 0;
 
         for(int i = 0;i < n;i++) {
             if(!visited[i]) {
                 count++;
-                dfs(adj, i, visited);
+                dfs(isConnected, i, visited);
             }
         }
         return count;
     }
 
-    private void dfs(ArrayList<ArrayList<Integer>> adj,
+    private void dfs(int[][] isConnected,
                      int node,
                      boolean[] visited) {
         visited[node] = true;
 
-        for(int adjVertex: adj.get(node)) {
-            if(!visited[adjVertex]) dfs(adj, adjVertex, visited);
+        for (int i = 0;i < isConnected.length;i++) {
+            if (!visited[i] && isConnected[node][i] == 1) dfs(adj, adjVertex, visited);
         }
-    }
-    public ArrayList<ArrayList<Integer>> convertToAdjList(int[][] isConnected) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
-
-        for(int i = 0;i < isConnected[0].length;i++) {
-            ArrayList<Integer> list = new ArrayList<Integer>();
-            for(int k = 0;k < isConnected[0].length;k++) {
-                if(i != k && isConnected[i][k] == 1) {
-                    list.add(k);
-                }
-            }
-            adj.add(list);
-        }
-        return adj;
     }
 }
 
