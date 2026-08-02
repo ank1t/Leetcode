4bb9fdb546da0d0bd5bb716b37fa828549f46a06
Ankit Singh <159.ank159@gmail.com>
2026-08-02 12:27:58 -0400

    Rotten oranges - starter code

---
+++ Graphs/RottenOranges.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        return 0;
+    }
+}
+
+class Scratch {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[][] grid = {
+                {2,1,1},
+                {1,1,0},
+                {0,1,1}
+        };
+        sol.orangesRotting(grid);
+    }
+}
