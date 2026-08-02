fa25d08209b52c2d517aa62ee92eebf074ef95b7
Ankit Singh <159.ank159@gmail.com>
2026-08-02 11:13:15 -0400

    Floodfill - started

---
+++ Graphs/FloodFill.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+
+    }
+}
+
+class Scratch {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+    }
+}
