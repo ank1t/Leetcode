7a197f910c131f4f52bce95c28746bf5aea58370
Ankit Singh <159.ank159@gmail.com>
2026-08-02 15:55:11 -0400

    Rotten oranges without modifying original grid

--- Graphs/RottenOranges.java
+++ Graphs/RottenOranges.java
@@ -1,21 +1,23 @@
 import java.util.*;
 
 class Solution {
-    {
-        if(!checkIfThereAreAnyFreshOranges(grid)) return 0;
+    public int orangesRotting(int[][] grid) {
+        boolean[] visited = new boolean[grid.length * grid[0].length];
+        if(!checkIfThereAreAnyFreshOranges(grid, visited)) return 0;
         int minTime = 0;
-        Queue<PairTime> q = findAllRottenOranges(grid);
+
+        Queue<PairTime> q = findAllRottenOranges(grid, visited);
 
         while(!q.isEmpty()) {
             PairTime pair = q.poll();
             int row = pair.row;
             int col = pair.col;
             int time = pair.time;
-            if(grid[row][col] == 2 && time > 0) continue;
+            if(visited[row * grid[0].length + col] && time > 0) continue;
 
             minTime = Math.max(time, minTime);
 
-            grid[row][col] = 2;
+            visited[row * grid[0].length + col] = true;
 
             if(row - 1 >= 0 && grid[row - 1][col] == 1) {
                 q.add(new PairTime(row - 1, col, time + 1));
@@ -34,25 +36,28 @@ class Solution {
             }
         }
 
-        return checkIfThereAreAnyFreshOranges(grid) ? -1 : minTime;
+        return checkIfThereAreAnyFreshOranges(grid, visited) ? -1 : minTime;
     }
 
 
-    boolean checkIfThereAreAnyFreshOranges(int[][] grid) {
+    boolean checkIfThereAreAnyFreshOranges(int[][] grid, boolean[] visited) {
         for(int i = 0;i < grid.length;i++) {
             for(int j = 0;j < grid[0].length;j++) {
-                if(grid[i][j] == 1) return true;
+                if(grid[i][j] == 1 && !visited[i * grid[0].length + j]) return true;
             }
         }
         return false;
     }
 
-    Queue<PairTime> findAllRottenOranges(int[][] grid) {
+    Queue<PairTime> findAllRottenOranges(int[][] grid, boolean[] visited) {
         Queue<PairTime> list = new LinkedList<PairTime>();
 
         for(int i = 0;i < grid.length;i++) {
             for(int j = 0;j < grid[0].length;j++) {
-                if(grid[i][j] == 2) list.add(new PairTime(i, j, 0));
+                if(grid[i][j] == 2) {
+                    visited[i * grid[0].length + j] = true;
+                    list.add(new PairTime(i, j, 0));
+                }
             }
         }
         return list;
