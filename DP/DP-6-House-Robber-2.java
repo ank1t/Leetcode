51cd44b50115b8357366248a720b1b2bbba39180
Ankit Singh <159.ank159@gmail.com>
2026-09-18 18:04:35 -0400

    Space optimized solution for House robber 2

---
+++ DP/DP-6-House-Robber-2.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        if(nums.length == 2) return Math.max(nums[0], nums[1]);
+
+        int n = nums.length;
+        int[] arr = new int[n - 1];
+        for(int i = 0;i < n - 1;i++) {
+            arr[i] = nums[i];
+        }
+        int ans1 = findMax(arr);
+        for(int i = 1;i < n;i++) {
+            arr[i - 1] = nums[i];
+        }
+        int ans2 = findMax(arr);
+
+        return Math.max(ans1, ans2);
+    }
+
+    int findMax(int[] nums) {
+        int prev2 = nums[0];
+        int prev1 = Math.max(nums[0], nums[1]);
+
+        for(int i = 2;i < nums.length;i++) {
+            int pick = nums[i] + prev2;
+            int notPick = prev1;
+
+            int cur = Math.max(pick, notPick);
+            prev2 = prev1;
+            prev1 = cur;
+        }
+        return prev1;
+    }
+}
+
+class Scratch {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.rob(new int[]{}));
+    }
+}
