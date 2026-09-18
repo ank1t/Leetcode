2fbda709021824295c56bf92091fb3c742c720a9
Ankit Singh <159.ank159@gmail.com>
2026-09-18 14:55:56 -0400

    Frog jump - generalized solution

---
+++ DP/DP-4-Frog-Jump.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    int minCostBU(int[] height, int k) {
+        int n = height.length;
+        int[] dp = new int[n];
+
+        for(int i = 1;i < n;i++) {
+            int min = Integer.MAX_VALUE;
+            for(int j = 1;j <= k;j++) {
+                if (i - j >= 0) {
+                    min = Math.min(min, dp[i - j] + Math.abs(height[i] - height[i - j]));
+                } else break;
+            }
+            dp[i] = min;
+        }
+        return dp[n - 1];
+
+    }
+}
+
+class Scratch {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println();
+    }
+}
