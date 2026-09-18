87b4719897a81140f7110fbebd5e698bb5dd9dfd
Ankit Singh <159.ank159@gmail.com>
2026-09-18 18:10:35 -0400

    Space optimized solution with slight twist

--- DP/DP-6-House-Robber-2.java
+++ DP/DP-6-House-Robber-2.java
@@ -3,7 +3,6 @@ import java.util.*;
 class Solution {
     public int rob(int[] nums) {
         if(nums.length == 1) return nums[0];
-        if(nums.length == 2) return Math.max(nums[0], nums[1]);
 
         int n = nums.length;
         int[] arr = new int[n - 1];
@@ -20,10 +19,10 @@ class Solution {
     }
 
     int findMax(int[] nums) {
-        int prev2 = nums[0];
-        int prev1 = Math.max(nums[0], nums[1]);
+        int prev2 = 0;
+        int prev1 = nums[0];
 
-        for(int i = 2;i < nums.length;i++) {
+        for(int i = 1;i < nums.length;i++) {
             int pick = nums[i] + prev2;
             int notPick = prev1;
 
