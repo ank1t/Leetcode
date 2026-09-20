04e712aa8deebcec94493fac75b78e5442d15b69
Ankit Singh <159.ank159@gmail.com>
2026-09-19 21:20:37 -0400

    Optimized base condition thanks to a youtube video

--- DP/DP-7-Ninja-Training.java
+++ DP/DP-7-Ninja-Training.java
@@ -1,19 +1,12 @@
 class Solution {
     //Top down
     public int ninjaTraining(int n, int[][] points) {
-        int[][] dp = new int[n][4];
         return f(n - 1, 3, points);
     }
 
     int f(int day, int last, int[][] points) {
-        if(day == 0) {
-            int max = 0;
-            for(int i = 0;i < 3;i++) {
-                if(i != last) {
-                    max = Math.max(max, points[day][i]);
-                }
-            }
-            return max;
+        if(day == -1) {
+            return 0;
         }
 
         int max = 0;
@@ -22,7 +15,6 @@ class Solution {
                 max = Math.max(points[day][i] + f(day - 1, i, points), max);
             }
         }
-
         return max;
     }
 
@@ -33,31 +25,19 @@ class Solution {
     }
 
     int f(int day, int last, int[][] points, int[][] dp) {
-        if(day == 0) {
-            if(dp[day][last] != 0) return dp[day][last];
-
-            int max = 0;
-            for(int i = 0;i < 3;i++) {
-                if(i != last) {
-                    max = Math.max(max, points[day][i]);
-                }
-            }
-            return dp[day][last] = max;
+        if(day == -1) {
+            return 0;
         }
+
+        if(dp[day][last] != 0) return dp[day][last];
 
         int max = 0;
         for(int i = 0;i < 3;i++) {
             if (i != last) {
                 max = Math.max(points[day][i] + f(day - 1, i, points, dp), max);
-                dp[day][i] = max;
             }
         }
-
-        max = 0;
-        for(int i = 0;i < 3;i++) {
-            max = Math.max(max, dp[day][i]);
-        }
-        return max;
+        return dp[day][last] = max;
     }
 
     //Bottom up
@@ -66,16 +46,12 @@ class Solution {
     }
 
     int fBU(int day, int last, int[][] points, int n) {
-        if(day == n - 1) {
-            int maxL = 0;
-            for(int i = 0;i < 3;i++) {
-                if(i != last) maxL = Math.max(maxL, points[day][i]);
-            }
-            return maxL;
+        if(day == n) {
+            return 0;
         }
 
         int max = 0;
-        for(int i = 0;i < n;i++) {
+        for(int i = 0;i < 3;i++) {
             if(i != last) {
                 max = Math.max(max, points[day][i] + fBU(day + 1, i, points, n));
             }
@@ -83,17 +59,56 @@ class Solution {
         return max;
     }
 
-    //Bottom Up memoized
+    /*
+        Bottom Up memoized
+        dp[0][3] will contain the solution.
+     */
+    public int ninjaTrainingBUM(int n, int[][] points) {
+        int[][] dp = new int[n][4];
+        return fBUM(0, 3, points, n, dp);
+    }
+
+    int fBUM(int day, int last, int[][] points, int n, int[][] dp) {
+        if(day == n) {
+            return 0;
+        }
+
+        if(dp[day][last] != 0) return dp[day][last];
+        int max = 0;
+        for(int i = 0;i < 3;i++) {
+            if (i != last) {
+                max = Math.max(max, points[day][i] + fBUM(day + 1, i, points, n, dp));
+            }
+        }
+        return dp[day][last] = max;
+    }
+
+    //Tabulation
+    public int ninjaTrainingTab(int n, int[][] points) {
+        int[][] dp = new int[n][4];
+        dp[0][0] = Math.max(points[0][1], points[0][2]);
+        dp[0][1] = Math.max(points[0][0], points[0][2]);
+        dp[0][2] = Math.max(points[0][0], points[0][1]);
+        dp[0][3] = Math.max(Math.max(points[0][0], points[0][1]), points[0][2]);
+
+        for(int day = 1;day < n;day++) {
+            for(int task = 0;task < 4;task++) {
+
+            }
+        }
+        return 0;
+    }
 }
 
 class Scratch {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int[][] points1 = new int[][] {{1,2,5},{3,1,1},{3,3,3}};
-        int[][] points2 = new int[][] {{10,40,70},{20,50,80},{30,60,90}};
-        int[][] points3 = new int[][] {{18,11,19},{4,13,7},{1,8,13}};
-        int[][] points4 = new int[][] {{10,50,1},{5,100,1}};
+        int[][] points1 = new int[][] {{1,2,5},{3,1,1},{3,3,3}}; //11
+        int[][] points2 = new int[][] {{10,40,70},{20,50,80},{30,60,90}};//210
+        int[][] points3 = new int[][] {{18,11,19},{4,13,7},{1,8,13}};//45
+        int[][] points4 = new int[][] {{10,50,1},{5,100,1}};//110
+        int[][] points5 = new int[][] {{2,1,3},{3,4,6},{10,1,6},{8,3,7}};//25
 
-        System.out.println(sol.ninjaTrainingBU(2, points4));
+        System.out.println(sol.ninjaTrainingBUM(4, points5));
     }
 }
