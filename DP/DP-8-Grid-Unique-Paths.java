f60bd5ddc49cc326feddea2afe496b352725d6cd
Ankit Singh <159.ank159@gmail.com>
2026-09-20 21:06:11 -0400

    Space optimization approach

--- DP/DP-8-Grid-Unique-Paths.java
+++ DP/DP-8-Grid-Unique-Paths.java
@@ -45,7 +45,23 @@ class Solution {
     }
 
     //Space optimization
-
+    int f4(int m, int n) {
+        int[] dp = new int[n];
+        int[] temp = new int[n];
+        for(int i = 0;i < m;i++) {
+            for(int j = 0;j < n;j++) {
+                if(i == 0 && j == 0) temp[j] = 1;
+                else {
+                    int up = 0, left = 0;
+                    if(i > 0) up = dp[j];
+                    if(j > 0) left = temp[j - 1];
+                    temp[j] = up + left;
+                }
+            }
+            dp = temp;
+        }
+        return dp[n - 1];
+    }
 }
 
 class Scratch {
