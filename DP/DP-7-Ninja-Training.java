5171b95ae064ed2d867d46dc14044a58f13721ca
Ankit Singh <159.ank159@gmail.com>
2026-09-22 17:17:05 -0400

    Ninja training - Space optimization working code

--- DP/DP-7-Ninja-Training.java
+++ DP/DP-7-Ninja-Training.java
@@ -109,13 +109,14 @@ class Solution {
     //Space optimization
     public int ninjaTrainingSpaceOptimization(int n, int[][] points) {
         int[] dp = new int[4];
-        int[] temp = new int[4];
+
         dp[0] = Math.max(points[0][1], points[0][2]);
         dp[1] = Math.max(points[0][0], points[0][2]);
         dp[2] = Math.max(points[0][0], points[0][1]);
         dp[3] = Math.max(Math.max(points[0][0], points[0][1]), points[0][2]);
 
         for(int day = 1;day < n;day++) {
+            int[] temp = new int[4];
             for(int last = 0; last < 4; last++) {
                 int max = 0;
                 for(int task = 0;task < 3;task++) {
